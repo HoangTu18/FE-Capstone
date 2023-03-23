fad9eb750e77740f5d54d1f409bcda3c195c3a9c
Pham Hoang Tu <phoangtu2000@gmail.com>
2023-03-23 14:17:55 +0700

    check

---
+++ .history/src/ultil/request_20230309132107.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+const request = axios.create({
+  baseURL:
+    "https://tfsapiv1-env.eba-aagv3rp5.ap-southeast-1.elasticbeanstalk.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+request.interceptors.request.use(async (config) => {
+  return config;
+});
+
+export const get = async (path, option = {}) => {
+  const response = await request.get(path, option);
+  return response.data;
+};
+
+export const remove = async (path, option) => {
+  const response = await request.delete(path, option);
+  return response.data;
+};
+
+export default request;
